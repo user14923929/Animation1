0ac92c84850f8dd38e27c69ba7f44f3a07fa2605
IlliaCoder <153951865+user14923929@users.noreply.github.com>
2024-12-30 18:37:20 +0200

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+control.waitMicros(6000)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # # # # #
+    # # # # #
+    . # # # .
+    `)
+control.waitMicros(6000)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # # # # #
+    # . . . #
+    . # # # .
+    `)
+control.waitMicros(6000)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . . # . .
+    . # . # .
+    . . # . .
+    `)
+control.waitMicros(100000)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
